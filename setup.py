18c6b18fcc27b54ad8ab6ada44289c0ffb09bf46
Guillermo Reyes <grcarmenaty@gmail.com>
2020-02-27 13:16:25 +0100

    Added standard python package structure as per https://packaging.python.org/tutorials/packaging-projects/

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymodal",
+    version="0.0.1",
+    author="Guillermo Reyes Carmenaty",
+    author_email="grcarmenaty@gmail.com",
+    description="Modal analysis data management and storage tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/grcarmenaty/pymodal",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3.0 ",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
