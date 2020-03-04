e5dbfff4b073fa50bcc52921aa9d3e25d7d2824b
Guillermo Reyes <grcarmenaty@gmail.com>
2020-03-04 20:50:48 +0100

    Took file paths relative to my system, made pytest verbose"

--- tests/test_FRF/test_utils.py
+++ tests/test_FRF/test_utils.py
@@ -7,9 +7,6 @@ import scipy.io as sio
 
 
 def test_unpack_frf():
-    path = (r'C:\Users\GuillermoReyes\GoogleDrive'
-            r'\Trabajos\Doctorado\BibliotecaPython\pymodal\tests\data\mat_FRFs'
-            r'\00B_Ref_8.MAT')
     frf_baseline = sio.loadmat(os.path.normpath(path))
     frf_info = sio.whosmat(os.path.normpath(path))
     frf_info = frf_info[0]
@@ -17,11 +14,6 @@ def test_unpack_frf():
     frf = pymodal.unpack_FRF_mat(path=(
         Path(__file__).parent.parent / 'data' / 'mat_FRFs' / '00B_Ref_8.MAT'))
     assert (frf == frf_baseline).all()
-    frf_string = pymodal.unpack_FRF_mat((
-        r'C:\Users\GuillermoReyes\GoogleDrive'
-        r'\Trabajos\Doctorado\BibliotecaPython\pymodal\tests\data\mat_FRFs'
-        r'\00B_Ref_8.MAT'))
-    assert (frf_string == frf_baseline).all()
     assert isinstance(frf, np.ndarray)
     assert frf.shape[0] == 6401
     assert frf.shape[1] == 81
