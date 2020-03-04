249d948bc13c07305673d63bcf5df3ba46f24ddc
Guillermo Reyes <grcarmenaty@gmail.com>
2020-03-04 20:56:12 +0100

    Fixed missing variables in test_utils

--- tests/test_FRF/test_utils.py
+++ tests/test_FRF/test_utils.py
@@ -1,18 +1,16 @@
 import numpy as np
-import os
 from pathlib import Path
 import pymodal
-import pytest
 import scipy.io as sio
 
 
 def test_unpack_frf():
-    frf_baseline = sio.loadmat(os.path.normpath(path))
-    frf_info = sio.whosmat(os.path.normpath(path))
+    path = Path(__file__).parent.parent / 'data' / 'mat_FRFs' / '00B_Ref_8.MAT'
+    frf_baseline = sio.loadmat(path)
+    frf_info = sio.whosmat(path)
     frf_info = frf_info[0]
     frf_baseline = frf_baseline[frf_info[0]]
-    frf = pymodal.unpack_FRF_mat(path=(
-        Path(__file__).parent.parent / 'data' / 'mat_FRFs' / '00B_Ref_8.MAT'))
+    frf = pymodal.unpack_FRF_mat(path=path)
     assert (frf == frf_baseline).all()
     assert isinstance(frf, np.ndarray)
     assert frf.shape[0] == 6401
