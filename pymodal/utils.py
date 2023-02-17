e01ba38be0b087a6862f54ea2d8133a1d1be3bf2
Guillermo Reyes Carmenaty <grcarmenaty@gmail.com>
2023-02-17 11:19:05 +0100

    Changed measurements for amplitude

--- pymodal/utils.py
+++ pymodal/utils.py
@@ -9,20 +9,20 @@ from typing import Optional
 # instead. Don't use preceding _ in any combination except in those cases.
 
 
-def __check_domain_amplitude_pair(
+def __check_domain_measurements_pair(
     domain_array: npt.NDArray[np.float64],
-    amplitude_array: npt.NDArray[np.complex64],
+    measurements_array: npt.NDArray[np.complex64],
 ):
-    """An auxiliary function that makes sure that a given domain and amplitude pair are
-    adequately typed, formatted and matched.
+    """An auxiliary function that makes sure that a given domain and measurements pair
+    are adequately typed, formatted and matched.
 
     Parameters
     ----------
     domain_array: numpy array of floats
         An array containing the temporal dimension, which measures the rate of physical
         change, be it by using domain, frequency or any other suitable quantity.
-    amplitude_array: numpy array of complex
-        An array with the amplitude of the signal recorded along the domain array.
+    measurements_array: numpy array of complex
+        An array with the measurements of the signal recorded along the domain array.
 
     Returns
     -------
@@ -31,26 +31,26 @@ def __check_domain_amplitude_pair(
         physical change, be it by using domain, frequency or any other suitable
         quantity.
     A numpy array of complexes
-        An array with the amplitude of the signal recorded along the domain array, with
-        the values corresponding to the values of the domain array.
+        An array with the measurements of the signal recorded along the domain array,
+        with the values corresponding to the values of the domain array.
     """
     domain_array = np.asarray(domain_array)
-    amplitude_array = np.asarray(amplitude_array)
-    if domain_array.shape[0] != amplitude_array.shape[0]:
+    measurements_array = np.asarray(measurements_array)
+    if domain_array.shape[0] != measurements_array.shape[0]:
         raise ValueError(
-            "Both the domain array and the amplitude array should be of the same length"
-            " along their first axis."
+            "Both the domain array and the measurements array should be of the same"
+            " length along their first axis."
         )
     if np.all(np.diff(domain_array) <= 0):
         raise ValueError(
             "Domain array values should be strictly increasing and non-repeating."
         )
-    return domain_array, amplitude_array
+    return domain_array, measurements_array
 
 
 def change_domain_resolution(
     domain_array: npt.NDArray[np.float64],
-    amplitude_array: npt.NDArray[np.complex64],
+    measurements_array: npt.NDArray[np.complex64],
     new_resolution: float,
 ):
     """Change the temporal resolution of an array of signals, assuming the temporal
@@ -61,8 +61,8 @@ def change_domain_resolution(
     domain_array: numpy array of floats
         An array containing the temporal dimension, which measures the rate of physical
         change, be it by using domain, frequency or any other suitable quantity.
-    amplitude_array: numpy array of complex
-        An array with the amplitude of the signal recorded along the domain array.
+    measurements_array: numpy array of complex
+        An array with the measurements of the signal recorded along the domain array.
     new_resolution: float
         The desired distance between any two adjacent values of the domain array.
 
@@ -73,13 +73,13 @@ def change_domain_resolution(
         physical change, be it by using domain, frequency or any other suitable
         quantity; with the new temporal resolution.
     A numpy array of complexes
-        An array with the new amplitude of the signal recorded along the domain array,
-        with the values corresponding to the values of the new domain array.
+        An array with the new measurements of the signal recorded along the domain
+        array, with the values corresponding to the values of the new domain array.
 
     """
 
-    domain_array, amplitude_array = __check_domain_amplitude_pair(
-        domain_array, amplitude_array
+    domain_array, measurements_array = __check_domain_measurements_pair(
+        domain_array, measurements_array
     )
     new_resolution = float(new_resolution)
     new_domain_array = np.arange(
@@ -123,30 +123,32 @@ def change_domain_resolution(
         )
         # Interpolate the values for each signal according to the new domain
         # vector
-        amplitude_shape = list(amplitude_array.shape)
-        amplitude_shape[0] = new_domain_array.shape[0]
-        amplitude_shape = tuple(amplitude_shape)
-        flat_amplitude = amplitude_array.reshape((amplitude_array.shape[0], -1))
-        new_amplitude_array = np.empty(
-            (new_domain_array.shape[0], flat_amplitude.shape[-1]),
-            dtype=amplitude_array.dtype,
+        measurements_shape = list(measurements_array.shape)
+        measurements_shape[0] = new_domain_array.shape[0]
+        measurements_shape = tuple(measurements_shape)
+        flat_measurements = measurements_array.reshape(
+            (measurements_array.shape[0], -1)
         )
-        for i in range(flat_amplitude.shape[-1]):
-            new_amplitude_array[:, i] = np.interp(
-                new_domain_array, domain_array, flat_amplitude[:, i]
+        new_measurements_array = np.empty(
+            (new_domain_array.shape[0], flat_measurements.shape[-1]),
+            dtype=measurements_array.dtype,
+        )
+        for i in range(flat_measurements.shape[-1]):
+            new_measurements_array[:, i] = np.interp(
+                new_domain_array, domain_array, flat_measurements[:, i]
             )
-        new_amplitude_array = new_amplitude_array.reshape(amplitude_shape)
+        new_measurements_array = new_measurements_array.reshape(measurements_shape)
     else:
         # Keep values corresponding to the new temporal resolution
         step = int(new_resolution / resolution)
-        new_amplitude_array = amplitude_array[0::step, ...]
+        new_measurements_array = measurements_array[0::step, ...]
 
-    return new_domain_array, new_amplitude_array
+    return new_domain_array, new_measurements_array
 
 
 def change_domain_span(
     domain_array: npt.NDArray[np.float64],
-    amplitude_array: npt.NDArray[np.complex64],
+    measurements_array: npt.NDArray[np.complex64],
     new_min_domain: Optional[float] = None,
     new_max_domain: Optional[float] = None,
 ):
@@ -158,8 +160,8 @@ def change_domain_span(
     domain_array: numpy array of floats
         An array containing the temporal dimension, which measures the rate of physical
         change, be it by using domain, frequency or any other suitable quantity.
-    amplitude_array: numpy array of complex
-        An array with the amplitude of the signal recorded along the domain array.
+    measurements_array: numpy array of complex
+        An array with the measurements of the signal recorded along the domain array.
     new_min_domain: float, default None
         The desired new minimum value for the domain array.
     new_max_domain: float, default None
@@ -172,13 +174,13 @@ def change_domain_span(
         physical change, be it by using domain, frequency or any other suitable quantity
         with the new temporal resolution.
     A numpy array of complexes
-        An array with the new amplitude of the signal recorded along the domain array,
-        with the values corresponding to the values of the new domain array.
+        An array with the new measurements of the signal recorded along the domain
+        array, with the values corresponding to the values of the new domain array.
 
     """
 
-    domain_array, amplitude_array = __check_domain_amplitude_pair(
-        domain_array, amplitude_array
+    domain_array, measurements_array = __check_domain_measurements_pair(
+        domain_array, measurements_array
     )
     # Make sure both new max and min domains exist
     if new_min_domain is None:
@@ -190,7 +192,7 @@ def change_domain_span(
     resolution = np.average(domain_diff)
     # Create copies of inputs to work on them, there are problems if both a new min and
     # max domain values are given otherwise
-    new_amplitude_array = np.copy(amplitude_array)
+    new_measurements_array = np.copy(measurements_array)
     new_domain_array = np.copy(domain_array)
     # Add a tail of 0s if max domain is greater than the current max domain
     if new_max_domain > new_domain_array[-1]:
@@ -206,13 +208,15 @@ def change_domain_span(
                 domain_extension = domain_extension[:-1]
             warn("Max domain will be changed to keep sample rate constant", UserWarning)
         new_domain_array = np.hstack((new_domain_array, domain_extension))
-        # Add as many amplitude points as domain points were created
-        amplitude_extension_shape = list(new_amplitude_array.shape)
-        amplitude_extension_shape[0] = domain_extension.shape[0]
-        amplitude_extension = np.zeros(
-            tuple(amplitude_extension_shape), dtype=amplitude_array.dtype
+        # Add as many measurements points as domain points were created
+        measurements_extension_shape = list(new_measurements_array.shape)
+        measurements_extension_shape[0] = domain_extension.shape[0]
+        measurements_extension = np.zeros(
+            tuple(measurements_extension_shape), dtype=measurements_array.dtype
         )
-        new_amplitude_array = np.hstack((new_amplitude_array, amplitude_extension))
+        new_measurements_array = np.hstack(
+            (new_measurements_array, measurements_extension)
+        )
     else:
         max_domain_index = (np.abs(new_domain_array - new_max_domain)).argmin()
         new_domain_array = new_domain_array[0:max_domain_index]
@@ -223,7 +227,7 @@ def change_domain_span(
                 new_domain_array = new_domain_array[:-1]
             warn("Max domain will be changed to keep sample rate constant", UserWarning)
         # Cut the signals to the new max domain
-        new_amplitude_array = new_amplitude_array[0:max_domain_index, ...]
+        new_measurements_array = new_measurements_array[0:max_domain_index, ...]
 
     # Add a head of 0s to the signals if the new min domain is smaller than
     # the precious min domain
@@ -243,13 +247,15 @@ def change_domain_span(
         new_domain_array = np.hstack((domain_extension, new_domain_array))
         if new_domain_array[0] < 0:
             new_domain_array = new_domain_array + abs(new_domain_array[0])
-        # Add as many amplitude points as domain points were created
-        amplitude_extension_shape = list(new_amplitude_array.shape)
-        amplitude_extension_shape[0] = domain_extension.shape[0]
-        amplitude_extension = np.zeros(
-            tuple(amplitude_extension_shape), dtype=amplitude_array.dtype
+        # Add as many measurements points as domain points were created
+        measurements_extension_shape = list(new_measurements_array.shape)
+        measurements_extension_shape[0] = domain_extension.shape[0]
+        measurements_extension = np.zeros(
+            tuple(measurements_extension_shape), dtype=measurements_array.dtype
         )
-        new_amplitude_array = np.hstack((amplitude_extension, new_amplitude_array))
+        new_measurements_array = np.hstack(
+            (measurements_extension, new_measurements_array)
+        )
     else:
         min_domain_index = (np.abs(new_domain_array - new_min_domain)).argmin()
         new_domain_array = new_domain_array[min_domain_index:]
@@ -258,8 +264,8 @@ def change_domain_span(
         if np.allclose(new_domain_array[0], new_min_domain):
             warn("Min domain will be changed to keep sample rate constant", UserWarning)
         # Cut the signal from the new min domain
-        new_amplitude_array = new_amplitude_array[min_domain_index:, ...]
-    return new_domain_array, new_amplitude_array
+        new_measurements_array = new_measurements_array[min_domain_index:, ...]
+    return new_domain_array, new_measurements_array
 
 
 if __name__ == "__main__":
@@ -267,35 +273,36 @@ if __name__ == "__main__":
 
     # Create a sinusoidal signal from a domain array
     domain_array = np.arange(0, 2 * np.pi + np.pi / 200, np.pi / 100)
-    amplitude_array = np.sin(domain_array)
-    # Change the resolution to half the original resolution to test change_resolution
-    new_domain_array, new_amplitude_array = change_resolution(
+    measurements_array = np.sin(domain_array)
+    # Change the resolution to half the original resolution to test
+    # change_domain_resolution
+    new_domain_array, new_measurements_array = change_domain_resolution(
         domain_array=domain_array,
-        amplitude_array=amplitude_array,
+        measurements_array=measurements_array,
         new_resolution=np.pi / 10,
     )
     # You should be able to see the difference in the peaks.
-    plt.plot(domain_array, amplitude_array)
-    plt.plot(new_domain_array, new_amplitude_array)
+    plt.plot(domain_array, measurements_array)
+    plt.plot(new_domain_array, new_measurements_array)
     plt.show()
 
     # Use the previously created sinusoidal signal and domain and change_domain_span
     # first to extend it by a period after and before
-    extended_domain_array, extended_amplitude_array = change_domain_span(
+    extended_domain_array, extended_measurements_array = change_domain_span(
         domain_array=domain_array,
-        amplitude_array=amplitude_array,
+        measurements_array=measurements_array,
         new_min_domain=-2 * np.pi,
         new_max_domain=4 * np.pi,
     )
     # then to cut it to the middle half of the period
-    cut_domain_array, cut_amplitude_array = change_domain_span(
+    cut_domain_array, cut_measurements_array = change_domain_span(
         domain_array=domain_array,
-        amplitude_array=amplitude_array,
+        measurements_array=measurements_array,
         new_min_domain=np.pi / 2,
         new_max_domain=3 * np.pi / 2,
     )
     # You should be able to see two overlapping signals and a displaced one
-    plt.plot(domain_array, amplitude_array)
-    plt.plot(extended_domain_array, extended_amplitude_array)
-    plt.plot(cut_domain_array, cut_amplitude_array)
+    plt.plot(domain_array, measurements_array)
+    plt.plot(extended_domain_array, extended_measurements_array)
+    plt.plot(cut_domain_array, cut_measurements_array)
     plt.show()
