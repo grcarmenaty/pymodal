8946372fa3291ccad77fa05530e949a62d6510c3
Guillermo Reyes Carmenaty <grcarmenaty@gmail.com>
2023-03-14 09:06:15 +0100

    Finished commenting frf

--- pymodal/frf.py
+++ pymodal/frf.py
@@ -21,7 +21,8 @@ class frf(_signal):
         label: Optional[str] = None,
     ):
         """Class designed to store all vibrational frequency response function (FRF)
-        information measured from a three-dimensional body, along with the spatial information related to each of the aforementioned FRFs.
+        information measured from a three-dimensional body, along with the spatial
+        information related to each of the aforementioned FRFs.
 
         Parameters
         ----------
@@ -42,26 +43,28 @@ class frf(_signal):
             How many degrees of freedom have been measured and are stored within the
             instance of this class, by default None.
         freq_start : float, optional
-            Starting frequency value, by default 0
+            Starting frequency value, by default 0.
         freq_end : float, optional
-            Maximum frequency value, by default None
+            Maximum frequency value, by default None.
         freq_span : float, optional
             Difference between the maximum and minimum values of frequency, by
-            default None
+            default None.
         freq_resolution : float, optional
-            Frequential resolution, by default None
+            Frequential resolution, by default None.
         measurements_units : Optional[str], optional
             Units used for the measurements stored within the instance of this class,
             they are assumed to be Newtons, millimeters and seconds, by default
-            "millimeter / (newton * second ** 2)"
+            "millimeter / (newton * second ** 2)".
         space_units : Optional[str], optional
-            Units used for the spatial coordinates of the degrees of freedom, by default "millimeter"
+            Units used for the spatial coordinates of the degrees of freedom, by
+            default "millimeter".
         method : str, optional
             Whether the method used to get the measurements is Multiple Input Single
             Output (MISO), Single Input Multiple Output (SIMO) or Multiple Input
-            Multiple Output (MIMO), by default "SIMO"
+            Multiple Output (MIMO), by default "SIMO".
         label : Optional[str], optional
-            An identifying label for the measurements stored in this instance, by default None
+            An identifying label for the measurements stored in this instance, by 
+            default None.
         """
         assert method in ["MISO", "SIMO", "MIMO"]
         super(frf, self).__init__(
@@ -87,11 +90,48 @@ class frf(_signal):
     def change_freq_span(
         self, new_min_freq: Optional[float] = None, new_max_freq: Optional[float] = None
     ):
+        """Either extend or cut the measured data according to new maximum and minimum
+        frequency values.
+
+        Parameters
+        ----------
+        new_min_freq : float, optional
+            The new desired minimum frequency. If negative, frequency will be added
+            before the initial frequency with all measurements set to 0, and the new
+            frequency origin will be set as the new 0, by default None.
+        new_max_freq : float, optional
+            The new desired maximum frequency. If greater than the previous max
+            frequency, frequency will be added after the previous max frequency with all
+            measurements set to 0, by default None.
+
+        Returns
+        -------
+        frf class object
+            A hard copy of the class instance with the modifications pertinent to the
+            method applied: the new frequency array without the values that fall outside
+            the given range, and extended as necessary to comply with the given range,
+            with the corresponding measurements values.
+        """
         return super().change_domain_span(
             new_min_domain=new_min_freq, new_max_domain=new_max_freq
         )
 
     def change_freq_resolution(self, new_resolution):
+        """Change the frequential resolution and interpolate as needed to return an
+        object with values coherent with the desired frequential resolution.
+
+        Parameters
+        ----------
+        new_resolution : float
+            The desired frequential resolution.
+
+        Returns
+        -------
+        timeseries class object
+            A hard copy of the class instance with the modifications pertinent to the
+            method applied: all values coherent with the desired new frequential
+            resolution.
+        """
         return super().change_domain_resolution(new_resolution=new_resolution)
 
 
